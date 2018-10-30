fc9dbb3166a460c61166de3f393e56d7609a1a2a
Tanner Brown <31678976+tannerdanger@users.noreply.github.com>
2018-10-29 20:46:43 -0700

    /weather endpoints work /login and /register are waiting on DB urls still need notification pinging, chat, contact, etc...

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,8 @@
 const express = require('express');
 
 //Create connection to Heroku Database
-let db = require('../util/utils').db;
-let getHash = require('../util/utils').getHash;
+//TODO: [uncomment] let db = require('../util/utils').db;
+//TODO: [uncomment] let getHash = require('../util/utils').getHash;
 
 var router = express.Router();
 
@@ -15,40 +15,57 @@ router.post('/', (req, res) => {
     let email = req.body['email'];
     let theirPw = req.body['password'];
     let wasSuccessful = false;
+    
+
     if(email && theirPw) {
-        //Using the 'one' method means that only one row should be returned
-        db.one('SELECT Password, Salt FROM Members WHERE Email=$1', [email])
-        //If successful, run function passed into .then()
-            .then(row => {
-                let salt = row['salt'];
-                //Retrieve our copy of the password
-                let ourSaltedHash = row['password'];
 
-                //Combined their password with our salt, then hash
-                let theirSaltedHash = getHash(theirPw, salt);
-
-                //Did our salted hash match their salted hash?
-                let wasCorrectPw = ourSaltedHash === theirSaltedHash;
-
-                //Send whether they had the correct password or not
-                res.send({
-                    success: wasCorrectPw
-                });
-            })
-            //More than one row shouldn't be found, since table has constraint on it
-            .catch((err) => {
-                //If anything happened, it wasn't successful
-                res.send({
-                    success: false,
-                    message: err
-                });
-            });
-    } else {
+        res.send({
+            success: true,
+            message: "login test without databasse success"
+        })
+    }
+    else {
         res.send({
             success: false,
-            message: 'missing credentials'
-        });
+            message: "no given email or password"
+        })
     }
+
+    //TODO: Uncomment below when database is connected
+    // if(email && theirPw) {
+    //     //Using the 'one' method means that only one row should be returned
+    //     //TODO: [uncomment] db.one('SELECT Password, Salt FROM Members WHERE Email=$1', [email])
+    //     //If successful, run function passed into .then()
+    //         .then(row => {
+    //             let salt = row['salt'];
+    //             //Retrieve our copy of the password
+    //             let ourSaltedHash = row['password'];
+    //
+    //             //Combined their password with our salt, then hash
+    //             let theirSaltedHash = getHash(theirPw, salt);
+    //
+    //             //Did our salted hash match their salted hash?
+    //             let wasCorrectPw = ourSaltedHash === theirSaltedHash;
+    //
+    //             //Send whether they had the correct password or not
+    //             res.send({
+    //                 success: wasCorrectPw
+    //             });
+    //         })
+    //         //More than one row shouldn't be found, since table has constraint on it
+    //         .catch((err) => {
+    //             //If anything happened, it wasn't successful
+    //             res.send({
+    //                 success: false,
+    //                 message: err
+    //             });
+    //         });
+    // } else {
+    //     res.send({
+    //         success: false,
+    //         message: 'missing credentials'
+    //     });
+    // }
 });
 
 module.exports = router;
---
+++ routes/weather.js
@@ -0,0 +1,91 @@
+const API_KEY = process.env.WEATHER_KEY;
+
+//express is the framework we're going to use to handle requests
+const express = require('express');
+
+//request module is needed to make a request to a web service
+const request = require('request');
+
+
+//retrieve the router object from express
+var router = express.Router();
+
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+router.use(bodyParser.json());
+
+//add a post route to the router.
+router.post("/current", (req, res) => {
+    console.log(req.body);
+    let lat = req.body['lat'];
+     let lon = req.body['lon'];
+     //base values in case lat/lon is empty
+     if(!lat || !lon) {
+        lat = 47.2098;
+        lon = -122.4090;
+    }
+    let url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${API_KEY}`
+
+    console.log(url);
+
+    request(url, function(error, response, body) {
+        if(error){
+            res.send(error);
+        } else {
+            res.send(body);
+        }
+    });
+});
+
+//add a get route to the router.
+router.post("/day", (req, res) => {
+
+    console.log(req.body);
+    let lat = req.body['lat'];
+    let lon = req.body['lon'];
+    //base values in case lat/lon is empty
+    if(!lat || !lon) {
+        lat = 47.2098;
+        lon = -122.4090;
+    }
+
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=1&key=${API_KEY}`
+
+    console.log(url);
+
+    request(url, function(error, response, body) {
+        if(error){
+            res.send(error);
+        } else {
+            res.send(body);
+        }
+    });
+});
+
+
+router.post("/tenday", (req, res) => {
+    console.log(req.body);
+    let lat = req.body['lat'];
+    let lon = req.body['lon'];
+    //base values in case lat/lon is empty
+    if(!lat || !lon) {
+        lat = 47.2098;
+        lon = -122.4090;
+    }
+
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=10&key=${API_KEY}`
+
+    console.log(url);
+
+    request(url, function(error, response, body) {
+        if(error){
+            res.send(error);
+        } else {
+            res.send(body);
+        }
+    });
+});
+
+
+// "return" the router
+module.exports = router;
